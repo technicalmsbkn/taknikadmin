6f7857f472ed9adae132520ba51617774e8f09a6
technicalmsbkn <moolchand390@gmail.com>
2020-09-11 21:45:48 +0530

    test

---
+++ src/TaknikadminServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Moolchand\Taknikadmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class TaknikadminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views','taknikadmin');
+    }
+}
