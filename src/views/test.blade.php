fed82dee17e4397d68c261c69b3cb77fe5bdb06a
technicalmsbkn <moolchand390@gmail.com>
2020-09-11 22:30:08 +0530

    version added

--- src/views/test.blade.php
+++ src/views/test.blade.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Taknik Admin</title>
 </head>
 <body>
-	<h1>its working</h1>
+	<h1>Hello, this is my first package</h1>
 </body>
 </html>
